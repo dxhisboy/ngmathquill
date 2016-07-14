858cf41537909248da4fe5aa374c8c059a0754a4
Xiaohui Duan <xduan@aimonta.com>
2016-07-14 11:27:35 +0800

    demo version

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # ngmathquill
 A wrapper for mathquill in AngularJS
+
+##directives
+
+###math-scope
+The root scope containing mathquill elements.
+
+###math-field
+The element containing visible math symbols
+
+###math-button
+This element can call cmd on the math-field, having 3 attrs:
+
+`cmd`: the cmd to call.
+
+`latex`: latex text to be displayed on the button.
+
+`movement`: using 'LRUD' meaning "Left", "Right", "Up", "Down", indicating the cursor movement after the cmd is called.
+
+###latex-field
+The field displaying corresponding latex text.
---
+++ ngmathquill.js
@@ -0,0 +1,115 @@
+angular.module("MathQuillDirective", [])
+    .directive("mathScope", function(){
+	return {
+	    restrict: "E",
+	    transclude: true,
+	    scope: {},
+	    controller:["$scope", function($scope) {
+		this.getLatex = $scope.getLatex = function() {
+		    if ($scope.field)
+			return $scope.field.latex();
+		    return "";
+		};
+		$scope.setLatex = function(text) {
+		    if ($scope.field)
+			$scope.field.latex(text);
+		};
+		this.setField = function(field) {
+		    $scope.field = field;
+		};
+		this.keystroke = function(stroke) {
+		    $scope.field.keystroke(stroke);
+		}
+		this.cmd = $scope.cmd = function(cmd) {
+		    this.inProc = true;
+		    if ($scope.field)
+			$scope.field.cmd(cmd);
+		    this.inProc = false;
+		};
+		this.MQ = $scope.MQ = MathQuill.getInterface(2);
+		this.focus = $scope.focus = function(){
+		    console.log("focus");
+		    $scope.field.focus();
+		};
+	    }],
+	    template: "<ng-transclude></ng-transclude>"
+	};
+    })
+    .directive("mathField", function() {
+	return {
+	    restrict: "E",
+	    require: "^^mathScope",
+	    controller: ["$scope", function($scope) {
+
+	    }],
+	    template: "<span class='math-field'></span>",
+	    link: function(scope, element, attrs, scopeCtrl) {
+		console.log(element.children(".math-field"));
+		console.log(scopeCtrl);
+		console.log(scope.$parent.MQ);
+		var field = scopeCtrl.MQ.MathField(
+		    element.children(".math-field")[0], {
+			handlers:{
+			    edit: function() {
+				if (!scopeCtrl.inProc)
+				    scope.$digest();
+			    }
+			}
+		    });
+		scopeCtrl.setField(field);
+	    }
+	};
+    })
+    .directive("mathButton", function() {
+	return {
+	    restrict: "E",
+	    require: "^^mathScope",
+	    scope: {},
+	    controller: ["$scope", function($scope) {
+	    }],
+	    template: "<button ng-click='insert()'><span></span></button>",
+	    link: function(scope, element, attrs, scopeCtrl){
+		var span = element.find("span");
+		span.text(attrs.latex);
+		console.log(span);
+		scopeCtrl.MQ.StaticMath(span[0]);
+		scope.insert = function (){
+		    scopeCtrl.cmd(attrs.cmd);
+		    scopeCtrl.focus();
+		    if ("movement" in attrs){
+			var mov = attrs.movement;
+			for (var i = 0; i < mov.length; i ++)
+			    switch (mov[i]){
+			    case 'L':
+				scopeCtrl.keystroke("Left");
+				break;
+			    case 'R':
+				scopeCtrl.keystroke("Right");
+				break;
+			    case 'U':
+				scopeCtrl.keystroke("Up");
+				break;
+			    case 'D':
+				scopeCtrl.keystroke("Down");
+			    }
+		    }
+		}
+	    }
+	};
+    })
+    .directive("latexField", function() {
+	return {
+	    restrict: "E",
+	    require: "^^mathScope",
+	    template: "<span ng-bind='getLatex()'></span>",
+	    controller: ["$scope", function($scope) {
+		console.log($scope);
+	    }],
+	    link: function(scope, element, attrs, scopeCtrl){
+		console.log(scope.$parent.getLatex);
+		scope.getLatex = function(){
+		    return scopeCtrl.getLatex();
+		};
+	    }
+	};
+    });
